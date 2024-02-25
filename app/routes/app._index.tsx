4642ba733b2556c5b588919b8095aafe8cd64c39
Philipp Spiess <hello@philippspiess.com>
2024-02-25 17:14:46 +0100

    Add upload

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,13 +1,40 @@
-import { LoaderFunctionArgs, json } from "@remix-run/cloudflare"
-import { useLoaderData } from "@remix-run/react"
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  json,
+  redirect,
+  unstable_createMemoryUploadHandler,
+  unstable_parseMultipartFormData,
+} from "@remix-run/cloudflare"
+import { useFetcher, useLoaderData } from "@remix-run/react"
 import Nav, { navLoader } from "~/ui/Nav"
 
 export async function loader({ context, request }: LoaderFunctionArgs) {
   return json({ ...(await navLoader!({ context, request })) })
 }
 
+export async function action({ request, context }: ActionFunctionArgs) {
+  const uploadHandler = unstable_createMemoryUploadHandler({
+    maxPartSize: 500_000,
+  })
+  const formData = await unstable_parseMultipartFormData(request, uploadHandler)
+  const image = formData.get("img") as File
+
+  context.bucket.put(image.name, await image.arrayBuffer())
+
+  return redirect("/app")
+}
+
 export default function Index() {
   const { user } = useLoaderData<typeof loader>()
+  const fetcher = useFetcher()
 
-  return <Nav user={user} />
+  return (
+    <Nav user={user}>
+      <fetcher.Form method="post" encType="multipart/form-data">
+        <input type="file" name="img" accept="image/*" />
+        <button type="submit">Upload to S3</button>
+      </fetcher.Form>
+    </Nav>
+  )
 }
---
+++ app/routes/image.$.tsx
@@ -0,0 +1,21 @@
+import { LoaderFunctionArgs } from "@remix-run/cloudflare"
+
+export async function loader({ params, context, request }: LoaderFunctionArgs) {
+  const key = params["*"]!
+
+  const object = await context.bucket.get(key)
+
+  if (object === null) {
+    return new Response("Object Not Found", { status: 404 })
+  }
+
+  const headers = new Headers()
+  for (const [key, value] of Object.entries(object.httpMetadata ?? {})) {
+    headers.set(key, value)
+  }
+  headers.set("etag", object.httpEtag)
+
+  return new Response(object.body as unknown as BodyInit, {
+    headers,
+  })
+}
